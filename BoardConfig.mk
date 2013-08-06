CyanogenMod/android_device_lge_p880

selinux: Add tegra policies
Change-Id: I449139424c1002e4c266c451d9b96139cca054b0

BoardConfig.mk
@@ -77,3 +77,17 @@ BOARD_RIL_CLASS := ../../../device/lge/p880/ril/
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p880/recovery-gfx.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/lge/p880/recovery-keys.c
 BOARD_CHARGER_ENABLE_SUSPEND := true
+
+ifeq ($(HAVE_SELINUX),true)
+
+BOARD_SEPOLICY_DIRS := \
+    device/lge/p880/selinux
+
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    file.te \
+    device.te \
+    domain.te
+
+endif
+

LineageOS/android_device_htc_endeavoru

BoardConfig.mk
@@ -149,3 +149,13 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/endeavoru
+
+# SE Linux policies
+BOARD_SEPOLICY_DIRS := \
+    device/htc/endeavoru/selinux
+
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    file.te \
+    device.te \
+    domain.te